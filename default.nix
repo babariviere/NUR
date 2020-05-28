91143c53b0987323e677ff82c16525dbb82e24eb
Bastien Rivière <babathriviere@gmail.com>
2020-05-28 19:12:40 +0200

    Fix fetchzip missing

--- default.nix
+++ default.nix
@@ -1,27 +1,28 @@
-{ nurpkgs ? import <nixpkgs> {} # For nixpkgs dependencies used by NUR itself
+{ nurpkgs ? import <nixpkgs> { } # For nixpkgs dependencies used by NUR itself
   # Dependencies to call NUR repos with
-, pkgs ? null
-, repoOverrides ? { }
-}:
+, pkgs ? null, repoOverrides ? { } }:
 
 let
   manifest = (builtins.fromJSON (builtins.readFile ./repos.json)).repos;
-  lockedRevisions = (builtins.fromJSON (builtins.readFile ./repos.json.lock)).repos;
+  lockedRevisions =
+    (builtins.fromJSON (builtins.readFile ./repos.json.lock)).repos;
 
   inherit (nurpkgs) lib;
 
-  repoSource = name: attr: import ./lib/repoSource.nix {
-    inherit name attr manifest lockedRevisions lib;
-    inherit (nurpkgs) fetchgit fetchzip;
-  };
+  repoSource = name: attr:
+    import ./lib/repoSource.nix {
+      inherit name attr manifest lockedRevisions lib;
+      inherit (pkgs) fetchgit fetchzip;
+    };
 
-  createRepo = name: attr: import ./lib/evalRepo.nix {
-    inherit name pkgs lib;
-    inherit (attr) url;
-    src = repoSource name attr + "/" + (attr.file or "");
-  };
+  createRepo = name: attr:
+    import ./lib/evalRepo.nix {
+      inherit name pkgs lib;
+      inherit (attr) url;
+      src = repoSource name attr + "/" + (attr.file or "");
+    };
 
 in {
-  repos =  (lib.mapAttrs createRepo manifest) // repoOverrides;
+  repos = (lib.mapAttrs createRepo manifest) // repoOverrides;
   repo-sources = lib.mapAttrs repoSource manifest;
 }
